ad6ee7d74bc95984290896e42427c44a634b7f83
Manuel Ulate <fr3zs09@gmail.com>
2023-01-05 15:25:46 -0600

    actualizar useState

--- src/GifApp.jsx
+++ src/GifApp.jsx
@@ -2,10 +2,20 @@ import { useState } from 'react'
 
 export const GifApp = () => {
     const [categories, setCategories] = useState([ 'A', 'B' ]);
+
+    const handleAddCategorie = () => {
+        //Forma 1
+        //setCategories([ ...categories, 'Valorant' ]);
+        //Al réves
+        setCategories([ 'Valorant', ...categories ]);
+        //Forma 2
+        //setCategories( oldCategories => [  ...categories, 'Valorant' ] );
+    }
   return (
     <>
         <h1>Gif App</h1>
 
+        <button onClick={ handleAddCategorie } >Agregar</button>
         <ol>
             { categories.map( category => {
                 return <li key={ category }>{ category }</li>
