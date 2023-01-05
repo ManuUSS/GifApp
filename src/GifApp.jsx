77adae0d850a4d1bcff15c8654f5ba2b6111d813
Manuel Ulate <fr3zs09@gmail.com>
2023-01-05 15:20:37 -0600

    lista ordenada y useState

--- src/GifApp.jsx
+++ src/GifApp.jsx
@@ -1,9 +1,16 @@
-import React from 'react'
+import { useState } from 'react'
 
 export const GifApp = () => {
+    const [categories, setCategories] = useState([ 'A', 'B' ]);
   return (
     <>
         <h1>Gif App</h1>
+
+        <ol>
+            { categories.map( category => {
+                return <li key={ category }>{ category }</li>
+            })}
+        </ol>
     </>
   )
 }
