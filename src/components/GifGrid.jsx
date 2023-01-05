d1ec89d06503354924edd3af9c05782d4afc2b2f
Manuel Ulate <fr3zs09@gmail.com>
2023-01-05 16:37:50 -0600

    api request

--- src/components/GifGrid.jsx
+++ src/components/GifGrid.jsx
@@ -1,6 +1,27 @@
 
+
+const getGifs = async ( category ) => {
+    
+    const url = `https://api.giphy.com/v1/gifs/search?api_key=TiRGbnrLg18rMYPrDQ45W5Q3YRHBnaa4&q=${ category }&limit=10`;
+    const response = await fetch( url );
+    
+    const { data } = await response.json();
+    
+    const gifs = data.map( img => ({
+        id: img.id,
+        title: img.title,
+        url: img.images.downsized_medium.url
+    }));
+
+    return gifs;
+}
+
 export const GifGrid = ({ category }) => {
   
+
+
+    getGifs( category );
+
     return (
         <h3>{ category }</h3>
   
