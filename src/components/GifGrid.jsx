2aea6cf1c2a1b05dff9593e08f054f8e6669157b
Manuel Ulate <fr3zs09@gmail.com>
2023-01-05 20:03:27 -0600

    mostrando la lista de los gifs

--- src/components/GifGrid.jsx
+++ src/components/GifGrid.jsx
@@ -1,16 +1,29 @@
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
 import { getGifs } from '../apis/getGifs.js';
 
 
 export const GifGrid = ({ category }) => {
   
+    const [gifs, setGifs] = useState([]);
+
+    const getImages = async () => {
+        const newGifs = await getGifs( category );
+        setGifs( newGifs );
+    }
 
     useEffect(() => {
-        getGifs( category );
+        getImages();
     }, [ ])
     
 
     return (
-        <h3>{ category }</h3>
+        <>
+            <h3>{ category }</h3>
+            <ul>
+                { gifs.map( ({ id, title }) => (
+                    <li key={ id }>{ title }</li>
+                )) }
+            </ul>
+        </>
     )
 }
